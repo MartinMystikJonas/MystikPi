2c76bfd1f77f4e7a6714dd7be486947b8ec3d1cc
martinjonas <martin.jonas@growjob.com>
2017-04-08 18:04:12 +0200

    robot with network and display

--- MystikPi/Projects/MystikRobot/Main.py
+++ MystikPi/Projects/MystikRobot/Main.py
@@ -14,27 +14,27 @@ network = Network(networkPort)
 display = RgbDisplay()
 
 # init
-display.warning("Loading...\n"+network.getIpAddress()+":"+networkPort)
+display.warning("Loading...\n"+network.getIpAddress()+":"+str(networkPort))
 
 time.sleep(3)
 
 while True:
   print("Waiting for connection")
-  display.info("Ready\n"+network.getIpAddress()+":"+networkPort)
+  display.info("Ready\n"+network.getIpAddress()+":"+str(networkPort))
   network.waitForConnection()
   print("Connected")
-  display.success("Connected\n"+network.getIpAddress()+":"+networkPort)
+  display.success("Connected\n"+network.getIpAddress()+":"+str(networkPort))
   while True:
     try:
       command = network.read();
       if(command != None):
         print("Received '" + command + "'")
         network.write("Received '" + command + "'");
-        display.success(command+"\n"+network.getIpAddress()+":"+networkPort)
+        display.success(command+"\n"+network.getIpAddress()+":"+str(networkPort))
       else:
         time.sleep(1)
         print(".")
     except IOError:
       print("Connection lost")
-      display.warning("Connection lost\n"+network.getIpAddress()+":"+networkPort)
+      display.warning("Connection lost\n"+network.getIpAddress()+":"+str(networkPort))
       break
