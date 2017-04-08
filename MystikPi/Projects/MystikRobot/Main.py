d470d5b9733fda088ef3682b0658731764067b07
martinjonas <martin.jonas@growjob.com>
2017-04-08 17:17:45 +0200

    robot with network and display

--- MystikPi/Projects/MystikRobot/Main.py
+++ MystikPi/Projects/MystikRobot/Main.py
@@ -10,6 +10,7 @@ networkPort = 12346
 
 # configuration
 network = Network(networkPort)
+display = RgbDisplay()
 
 # init
 setRGB(255,0,0)
@@ -19,24 +20,21 @@ time.sleep(100)
 
 while True:
   print("Waiting for connection")
-  setRGB(255,0,0)
-  setText("Waiting for connection\n"+network.getIpAddress())
+  display.info("Waiting for connection\n"+network.getIpAddress())
   network.waitForConnection()
   print("Connected")
-  setRGB(0,255,0)
-  setText("Connected\n"+network.getIpAddress())
+  display.success("Connected\n"+network.getIpAddress())
   while True:
     try:
       command = network.read();
       if(command != None):
         print("Received '" + command + "'")
         network.write("Received '" + command + "'");
-        setText(message+"\n"+network.getIpAddress())
+        display.success(message+"\n"+network.getIpAddress())
       else:
         time.sleep(1)
         print(".")
     except IOError:
       print("Connection lost")
-      setRGB(255,0,0)
-      setText("Connected\n"+network.getIpAddress())
+      display.warning("Connected\n"+network.getIpAddress())
       break
